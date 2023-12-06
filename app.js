8e6a0a3772443c6c953a0f6c88ee85c403ac1d7a
Virus <146997559+Virus36010@users.noreply.github.com>
2023-12-06 16:35:42 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,16 +5,19 @@ let app = express()
 let server = require("http").createServer(app)
 let io = require("socket.io")(server)
 
-server.listen(3000)
-console.log('Server started!')
+let host = 5656
+server.listen(host)
+console.log('Server started! Host: ' + host)
 
-app.use(express.static("static"))
+app.use(express.static("public"))
 app.get("/", function(req, res){
-  res.sendFile(__dirname + "/help-chat.html")
+  res.sendFile(__dirname + "/public/help-chat.html")
 })
 
 let connections = 0
 io.sockets.on('connection', function(socket){
   connections++
-  console.log("Connection " + connections)
+  //console.log("Connection " + connections)
+  //console.log("Connected")
 })
+if (connections > 0){console.log("Connection " + connections)}
