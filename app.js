2a43d1bb252d252ee4f7d52c552eea9b6d974d24
Virus <146997559+Virus36010@users.noreply.github.com>
2023-12-06 18:11:11 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -9,15 +9,26 @@ let host = 5656
 server.listen(host)
 console.log('Server started! Host: ' + host)
 
+let users = []
+let connections = []
+
 app.use(express.static("public"))
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/public/help-chat.html")
 })
 
-let connections = 0
+let how_many_connections_was = 0
 io.sockets.on('connection', function(socket){
-  connections++
-  //console.log("Connection " + connections)
-  //console.log("Connected")
+  how_many_connections_was++
+  connections.push(socket);
+  console.log("User " + how_many_connections_was + " connected")
+
+  socket.on('disconnect', function(data){
+    connections.splice(connections.indexOf(socket),1);
+    console.log("User disconnected")
+  })
+
+  socket.on('send mess', function(data){
+    io.sockets.emit('add mess', {mess: data.mess, name:data.name})
+  })
 })
-if (connections > 0){console.log("Connection " + connections)}
