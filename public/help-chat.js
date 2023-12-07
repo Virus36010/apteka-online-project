fd369089f3fe01d49642c205316e5fb786ff3df5
Virus <146997559+Virus36010@users.noreply.github.com>
2023-12-07 19:31:08 +0300

    Add files via upload

---
+++ public/help-chat.js
@@ -0,0 +1,13 @@
+
+let homeButton = document.getElementById("homeButton")
+homeButton.addEventListener("click", function(){
+  window.location.href="apteca-main.html"
+})
+
+let headerButton2 = document.getElementById("headerButton2")
+let headerButton3 = document.getElementById("headerButton3")
+
+let profButton = document.getElementById("profile")
+profButton.addEventListener("click", function(){
+  
+})
